624a0987e09b059c02967d3b43b485e36a0cb661
musasazib <musasazib@gmail.com>
2021-09-24 12:48:02 +0600

    Simple React SPA

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,12 +7,19 @@ const Cart = (props) => {
     for (const product of cart) {
         total = total + product.price;
     }
+
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
             <h2>Order Summary</h2>
             <h4>Item Ordered: {props.cart.length}</h4>
             <br />
-            <p>Total: {total}</p>
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
